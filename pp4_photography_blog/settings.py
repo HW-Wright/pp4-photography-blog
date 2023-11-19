cb9c06be3dacb88ec87d3e20f4b6c97ae1676bf8
blahblahblah589 <hwright589@icloud.com>
2023-11-19 15:33:04 +0000

    enable deployment to heroku

--- pp4_photography_blog/settings.py
+++ pp4_photography_blog/settings.py
@@ -18,6 +18,7 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -29,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-blahblahbla-pp4photogra-h3lg2gdsojw.ws-us106.gitpod.io']
+ALLOWED_HOSTS = ['8000-blahblahbla-pp4photogra-h3lg2gdsojw.ws-us106.gitpod.io', 'pp4-photography-blog.heroku.com', 'localhost']
 
 
 # Application definition
@@ -61,7 +62,7 @@ ROOT_URLCONF = 'pp4_photography_blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
