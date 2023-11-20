949b0ea2d52de45e4563eb5518813338fb37015c
blahblahblah589 <hwright589@icloud.com>
2023-11-20 19:56:40 +0000

    create model for user posts and user comments, migrate

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((False, 'Draft'), (True, 'Published'))
+
+class Post(models.Model):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    image = CloudinaryField('image')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photo_post')
+    caption = models.TextField()
+    location = models.TextField()
+    likes = models.ManyToManyField(User, related_name='photo_likes', blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    status = models.BooleanField(choices=STATUS, default=False)
+
+    class Meta:
+        ordering = ['-date_created']
+
+    def __str__(self):
+        return self.title
+
+    def likes_count(self):
+        return self.likes.count()
+
+class Comments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='photo_comment')
+    comment_content = models.TextField()
+    created_by = models.CharField(max_length=50)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date_created']
+
+    def __str__(self):
+        return f'{self.comment_content} from {self.created_by}'
