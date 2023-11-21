e4ff0368c810a8370f3c030bf1de85900359a029
blahblahblah589 <hwright589@icloud.com>
2023-11-21 20:27:00 +0000

    add class view and link url, create bootstrap structure for index.html, add style

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Post, Comment
 
+
+# def featured_posts(request):
+#     posts = Post.objects.all()
+#     context = {
+#         'posts' : posts
+#     }
+#     return render(request, '/templates/index.html/', context)
+
+
 class FeaturedPosts(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=True).order_by('-date_created')
