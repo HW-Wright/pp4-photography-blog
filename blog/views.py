6f0c5786b3141fd3e800de109c068261c2a01607
blahblahblah589 <hwright589@icloud.com>
2023-11-21 18:31:49 +0000

    amend comment model to have singular name, create assets folder for css and images, create index.html template, create style.css, add bootstrap and font awesome links

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post, Comment
 
-# Create your views here.
+class FeaturedPosts(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=True).order_by('-date_created')
+    template_name = 'index.html'
+    paginate_by = 9
