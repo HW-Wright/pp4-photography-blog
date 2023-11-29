1ac1224a4d14a50282e32fd5634f267e875a91b2
blahblahblah589 <hwright589@icloud.com>
2023-11-29 19:55:52 +0000

    create SpecificPost and AllPosts views plus urls. Create template for viewing a single post and populate with HTML and Python. Create site navigation links

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.FeaturedPosts.as_view(), name='homepage'),
+    path('blog', views.AllPosts.as_view(), name='blog'),
+    path('<slug:slug>/', views.SpecificPost.as_view(), name='specific_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,34 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Post, Comment
-
-
-# def featured_posts(request):
-#     posts = Post.objects.order_by('date_created')[:3]
-#     context = {
-#         'posts' : posts
-#     }
-#     template = 'inex.html'
-#     return render(request, template, context)
 
 
 class FeaturedPosts(generic.ListView):
     model = Post
     queryset = Post.objects.filter(status=True).order_by('-date_created')[:3]
     template_name = 'index.html'
+
+class SpecificPost(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=True)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = Comment.objects.order_by('-date_created')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request, 
+            'specific_post.html',
+            {
+                'post': post,
+                'comments': comments,
+                'liked': liked
+            },
+        )
+
+class AllPosts(generic.ListView):
+    model = Post
+    queryset = Post.objects.filter(status=True).order_by('-date_created')
+    template_name = 'blog.html'
