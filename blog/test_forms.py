6818bad5c55c2bef88dac7fb89a0e45390038b81
blahblahblah589 <hwright589@icloud.com>
2023-12-06 23:15:17 +0000

    creae tests for forms and models

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,26 +1,45 @@
 from django.test import TestCase
-from .form import CommentForm, PostForm, DeleteForm, EditForm
+from .forms import CommentForm, PostForm, DeleteForm, EditForm
 
 class TestPostForm(TestCase):
+
+    """All posts must be uploaded with a title"""
 
     def test_title_required(self):
         form = PostForm({'title': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('title', form.errors)
 
+    """All posts must be uploaded with an image file"""
+
     def test_image_required(self):
         form = PostForm({'image': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('title', form.errors)
-    
-    def
 
 
-# class TestCommentForm(TestCase):
-    
+class TestCommentForm(TestCase):
 
-# class TestDeleteForm(TestCase):
-    
+    """The user cant submit an empty comment"""
 
-# class TestEditForm(TestCase):
-    
+    def test_comment_content_required(self):
+        form = CommentForm({'coment_content': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('comment_content', form.errors.keys())
+        self.assertEqual(form.errors['comment_content'][0], 'This field is required.')
+
+
+class TestEditForm(TestCase):
+
+    """The user should not be able to alter the slug field from this view"""
+
+    def test_slug_not_present(self):
+        form = EditForm()
+        self.assertNotIn('slug', form.fields)
+
+    """All posts must be uploaded with a title"""
+
+    def test_title_required(self):
+        form = PostForm({'title': ''})
+        self.assertFalse(form.is_valid())
+        self.assertIn('title', form.errors)
---
+++ blog/test_views.py
@@ -0,0 +1,66 @@
+from django.test import TestCase
+from django.urls import reverse
+from .models import User, Post, Comment, Editor
+from .views import FeaturedPosts, SpecificPost, LikePost, AllPosts, edit_post, add_post, delete_comment, delete_post
+
+
+class TestViews(TestCase):
+
+    """Test data"""
+    @classmethod
+    def setUpTestData(self):
+        self.user = User.objects.create(username='testuser')
+        self.user.set_password('welcome1')
+        self.user.save()
+
+        self.post = Post.objects.create(
+            title = 'test',
+            image = 'img.jpeg',
+            slug = 'test',
+            created_by = self.user,
+            status = True
+        )
+        self.post.save()
+
+        self.comment = Comment.objects.create(
+            post = self.post,
+            created_by = self.user,
+            comment_content = 'test',
+            comment_id = 1
+        )
+
+    """The view should produce the three posts with the most likes,
+    and render them in index.html.
+    """
+
+    def test_get_featured_posts(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    """The view should select the slug for the requested post and render
+    specific_post.html
+    """
+
+    def test_get_specific_post(self):
+        response = self.client.get(reverse('specific_post', args=[self.post.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'specific_post.html')
+
+    """This new should redirect the user to the edit_post template"""
+
+    def test_edit_post(self):
+        response = self.client.get(reverse('edit', args=[self.post.slug]))
+        self.assertEqual(response.status_code, 302)
+
+    """This view should redirect the user to the add_post template"""
+
+    def test_add_post(self):
+        response = self.client.get(reverse('add'))
+        self.assertEqual(response.status_code, 302)
+
+    """This view should redirect the user to the delete post form on delete_post.html"""
+
+    def test_delete_post(self):
+        response = self.client.get(reverse('delete_post', args=[self.post.slug]))
+        self.assertEqual(response.status_code, 302)
