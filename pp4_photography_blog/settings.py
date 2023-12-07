456a31efefe2060cc328dc2918464ac47c35ad36
blahblahblah589 <hwright589@icloud.com>
2023-12-07 12:17:06 +0000

    deployment commit

--- pp4_photography_blog/settings.py
+++ pp4_photography_blog/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'pp4-photography-blog-dfac9ff0d622.herokuapp.com',
